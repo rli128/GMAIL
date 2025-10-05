45495103276a4fe8e86a0efa208e31fe571e56e2
rli128 <114562309+rli128@users.noreply.github.com>
2025-10-05 00:47:30 -0700

    Initial commit

---
+++ emails.py
@@ -0,0 +1,31 @@
+import pickle
+import re
+from bs4 import BeautifulSoup
+import spacy
+import json
+import datetime
+from datetime import timezone
+import itertools
+
+with open("messages_cache.pkl", "rb") as f:
+    loaded_messages = pickle.load(f)
+
+def strip_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+    
+    for element in soup(['script', 'style', 'meta', 'link', 'title', 'head']):
+        element.decompose()
+    
+    text = soup.get_text(separator="\n", strip=True)
+    text = re.sub(r'\s+', ' ', text).strip()    
+    return text
+
+nlp = spacy.load('role_model')
+
+for msg in loaded_messages[:15]:
+    
+    subject_preview = strip_html(msg['subject'])
+    body_preview = strip_html(msg['body'][:300])
+    text = (subject_preview + " | " + body_preview)
+    text = re.sub(r"\s+", " ", text).strip()
+    print(text)
